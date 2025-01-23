e618ada292a12d321d1c822ae07f5a2ba2572ca8
Daniel Gomes Lacerda <72995219+ajtopper@users.noreply.github.com>
2025-01-22 23:15:58 -0300

    Teste

--- AppRPG.py
+++ AppRPG.py
@@ -4,6 +4,8 @@ from langchain.prompts import PromptTemplate
 import os
 import yaml
 
+
+
 with open('config.yaml', 'r') as config_file:
     config = yaml.safe_load(config_file)
 os.environ['OPENAI_API_KEY'] = config['OPENAI_API_KEY']
